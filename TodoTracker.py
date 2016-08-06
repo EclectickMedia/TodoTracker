76d90e93ec5f4942ca4140606ef62cb278c64ce4
EclectickEclipse <ariana.giroux@gmail.com>
2016-08-06 00:22:59 -0600

    Documentation Update

--- TodoTracker.py
+++ TodoTracker.py
@@ -21,8 +21,6 @@ import sys
 import time
 
 # TODO Include a Read me for the application.
-# TODO Add functionality to notate file path when printing the file name.
-# TODO TodoTracker is not skipping excluded paths.
 """ Preamble
 This software is intended to be a low overhead, simple, and pythonic method of searching for `# TODO' tags out of a
 software. Intended for Python, this software will also be able to search any file that has non-binary encoding.
